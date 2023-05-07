62c8267660ab12bf7c4852fb5578f25680a4b44f
Vijay Goswami <hexpit@gmail.com>
2023-05-07 19:43:53 +0530

    updated logviewer config to select adminator as default

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -40,7 +40,7 @@ return [
      |  Make your own theme by adding a folder to the views directory and specifying it here.
      */
 
-    'theme' => 'bootstrap-4',
+    'theme' => 'adminator',
 
     /* -----------------------------------------------------------------
      |  Route settings
