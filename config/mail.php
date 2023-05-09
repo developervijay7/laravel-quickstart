5faef6e8a11e15b63f2b8eca3eee0e82ee60975e
Vijay Goswami <hexpit@gmail.com>
2023-05-09 23:12:33 +0530

    updated dependencies

--- config/mail.php
+++ config/mail.php
@@ -35,15 +35,12 @@ return [
     */
 
     'mailers' => [
-        'smtp' => [
-            'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-        ],
+        'transport' => 'smtp',
+        'url' => env('MAIL_URL'),
+        'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+        'port' => env('MAIL_PORT', 587),
+        'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+
 
         'ses' => [
             'transport' => 'ses',
