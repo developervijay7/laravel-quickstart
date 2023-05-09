523c11ee823863c85bb27bc1dd3bd614340f2c15
Vijay Goswami <hexpit@gmail.com>
2023-05-09 17:41:56 +0530

    added redirect route to check where to redirect after login

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -14,5 +15,14 @@ class HomeController extends Controller
     public function contact()
     {
         return view('frontend.contact');
+    }
+
+    public function redirects()
+    {
+        if (Auth::user()->type == 'admin') {
+            return redirect(route('admin.dashboard'));
+        } else {
+            return redirect(route('frontend.user.dashboard'));
+        }
     }
 }
