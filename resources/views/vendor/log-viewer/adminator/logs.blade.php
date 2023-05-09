a038e93689f44b9fa9f3c966bce444ef2c83fe58
Vijay Goswami <hexpit@gmail.com>
2023-05-09 23:37:51 +0530

    added jquery to logviewer adminator theme fixed composer as per laravel 10 requirements

--- resources/views/vendor/log-viewer/adminator/logs.blade.php
+++ resources/views/vendor/log-viewer/adminator/logs.blade.php
@@ -78,6 +78,7 @@
 @endsection
 
 @push('before-logscripts')
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     <script>
         function deleteConfirm(element) {
             Swal.fire({
